1b1bea359ad56a25c50207299e2852d7a6dfcb0f
Donato Azevedo <donatoaz@gmail.com>
2020-12-12 18:44:02 -0300

    Day 2

---
+++ lib/day2/day2.ex
@@ -0,0 +1,53 @@
+defmodule Aoc2020.Day2 do
+  require Logger
+
+  defmodule Term do
+    defstruct min: "", max: "", character: "", password: ""
+  end
+
+  defp build_struct(list) do
+    regex = ~r/(?<min>\d+)-(?<max>\d+)\s*(?<character>\w): (?<password>\w+)/
+
+    list
+      |> Enum.map(fn e -> Regex.named_captures(regex, e) end) # parse string
+      |> Enum.map(fn e -> Map.new(e, fn {k, v} -> {String.to_atom(k), v} end) end) # convert keys to atoms
+      |> Enum.map(fn e -> struct(Term, e) end) # create a struct to make it easier to work
+  end
+
+  defp part_1_predicate(term) do
+    count = term.password
+      |> String.graphemes
+      |> Enum.count(& &1 == term.character)
+
+    count >= String.to_integer(term.min) && count <= String.to_integer(term.max)
+  end
+
+  defp part_2_predicate(term) do
+    # (Be careful; Toboggan Corporate Policies have no concept of "index zero"!)
+
+    a = String.at(term.password, String.to_integer(term.min) -1) == term.character
+    b = String.at(term.password, String.to_integer(term.max) -1) == term.character
+
+    (a && !b) || (!a && b)
+  end
+
+  def run(input) do
+    # Each line gives the password policy and then the password. The password policy indicates
+    #  the lowest and highest number of times a given letter must appear for the password to be valid.
+    #  For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+    terms = build_struct(input)
+
+    valid_terms = terms
+      |> Enum.filter(&part_1_predicate/1)
+
+    Enum.count(valid_terms)
+  end
+
+  def run_part_2(input) do
+    terms = build_struct(input)
+
+    valid_terms = terms |> Enum.filter(&part_2_predicate/1)
+
+    Enum.count(valid_terms)
+  end
+end
---
+++ test/day2/day2_test.exs
@@ -0,0 +1,36 @@
+defmodule Aoc2020test.Day2 do
+  require Logger
+  use ExUnit.Case
+  doctest Aoc2020.Day2
+
+  defp read_file_to_list(path) do
+    {:ok, content} = File.read(path)
+
+    content
+      |> String.split("\n", trim: true)
+  end
+
+  test "works for the provided sample" do
+    sample_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+
+    assert Aoc2020.Day2.run(sample_input) == 2
+  end
+
+  test "works for the provided list" do
+    input = read_file_to_list("./test/day2/input.txt")
+
+    assert Aoc2020.Day2.run(input) == 467
+  end
+
+  test "works for the sample input of part 2" do
+    sample_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+
+    assert Aoc2020.Day2.run_part_2(sample_input) == 1
+  end
+
+  test "works for the provided list on part 2" do
+    input = read_file_to_list("./test/day2/input.txt")
+
+    assert Aoc2020.Day2.run_part_2(input) == 441
+  end
+end
