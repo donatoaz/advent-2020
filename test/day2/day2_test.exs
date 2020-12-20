ce6b872ab0ffe68a3e212c1c4a8687576ab229bc
Donato Azevedo <donatoaz@gmail.com>
2020-12-20 19:09:44 -0300

    Refactor day 2 test

--- test/day2/day2_test.exs
+++ test/day2/day2_test.exs
@@ -1,36 +1,34 @@
 defmodule Aoc2020test.Day2 do
   require Logger
   use ExUnit.Case
+  import Helpers, only: [read_file_to_list: 1]
   doctest Aoc2020.Day2
 
-  defp read_file_to_list(path) do
-    {:ok, content} = File.read(path)
+  describe "part 1" do
+    test "works for the provided sample" do
+      sample_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
 
-    content
-    |> String.split("\n", trim: true)
+      assert Aoc2020.Day2.run(sample_input) == 2
+    end
+
+    test "works for the provided list" do
+      input = read_file_to_list("./test/day2/input.txt")
+
+      assert Aoc2020.Day2.run(input) == 467
+    end
   end
 
-  test "works for the provided sample" do
-    sample_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+  describe "part 2" do
+    test "works for the sample input" do
+      sample_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
 
-    assert Aoc2020.Day2.run(sample_input) == 2
-  end
+      assert Aoc2020.Day2.run_part_2(sample_input) == 1
+    end
 
-  test "works for the provided list" do
-    input = read_file_to_list("./test/day2/input.txt")
+    test "works for the provided list" do
+      input = read_file_to_list("./test/day2/input.txt")
 
-    assert Aoc2020.Day2.run(input) == 467
-  end
-
-  test "works for the sample input of part 2" do
-    sample_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
-
-    assert Aoc2020.Day2.run_part_2(sample_input) == 1
-  end
-
-  test "works for the provided list on part 2" do
-    input = read_file_to_list("./test/day2/input.txt")
-
-    assert Aoc2020.Day2.run_part_2(input) == 441
+      assert Aoc2020.Day2.run_part_2(input) == 441
+    end
   end
 end
